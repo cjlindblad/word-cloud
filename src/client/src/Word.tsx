3a98f93c0968f5fc4aafa1c06f3e64dbe423f7e1
cj lindblad <carljohan.lindblad@gmail.com>
2020-01-26 16:42:36 +0100

    Set a minimum font size

--- src/client/src/Word.tsx
+++ src/client/src/Word.tsx
@@ -9,9 +9,12 @@ interface Props {
 const Word = (props: Props) => {
   const { word } = props;
 
+  const MIN_FONT_SIZE = 0.5;
+  const fontSize = 5 * word.weight;
+
   const Wrapper = styled.div`
     position: absolute;
-    font-size: ${5 * word.weight}rem;
+    font-size: ${fontSize > MIN_FONT_SIZE ? fontSize : MIN_FONT_SIZE}rem;
     opacity: ${word.weight};
     margin-left: ${Math.random() * 500 - 250}px;
     margin-top: ${Math.random() * 300 - 150}px;
