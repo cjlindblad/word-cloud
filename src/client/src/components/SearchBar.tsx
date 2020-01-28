c55a7ceccf89a2ecaee1b90b62f92b51507a7976
cj lindblad <carljohan.lindblad@gmail.com>
2020-01-28 22:36:02 +0100

    npm script tweak

--- src/client/src/components/SearchBar.tsx
+++ src/client/src/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import searchIcon from '../icons/search.svg';
+import searchIcon from '../assets/search.svg';
 
 const Wrapper = styled.div`
   display: flex;
